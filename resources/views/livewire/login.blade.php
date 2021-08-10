7308d44938998c39ed06a974db01eb1ab82ba9d0
Epafroditus George <cavidjaja@gmail.com>
2021-08-10 14:03:36 +0700

    up

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,15 +1,15 @@
 @section('title', 'Login - My Finance')
 <div class="container">
     <!-- Outer Row -->
-    <div class="row justify-content-center">
+    <div class="row mobile justify-content-center">
         <div class="col-xl-10 col-lg-12 col-md-9">
             <div class="bg-gray-100 border-0 card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    <div class="row">
+                    <div class="row mobile">
                         <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                         <div class="col-lg-6">
-                            <div class="p-5">
+                            <div class="small-when-0 p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-white mb-4">Welcome Back!</h1>
                                 </div>
--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -3,10 +3,10 @@
     <div class="bg-gray-100 border-0 card o-hidden border-0 shadow-lg my-5">
         <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
-            <div class="row">
+            <div class="row mobile">
                 <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
                 <div class="col-lg-7">
-                    <div class="p-5">
+                    <div class="small-when-0 p-5">
                         <div class="text-center">
                             <h1 class="h4 text-white mb-4">Create an Account!</h1>
                         </div>
@@ -33,7 +33,7 @@
                                     </span>
                                 @enderror
                             </div>
-                            <div class="form-group row">
+                            <div class="form-group row mobile small-when-0 ">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <input type="password"
                                         class="border-0 form-control form-control-user @error('form.password') is-invalid @enderror"
@@ -45,7 +45,7 @@
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="col-sm-6">
+                                <div class="col-sm-6 mb-3">
                                     <input type="password" class="border-0 form-control form-control-user"
                                         wire:model="form.password_confirmation" name="password_confirmation" required
                                         autocomplete="new-password" id="myInput2" placeholder="Repeat Password">
