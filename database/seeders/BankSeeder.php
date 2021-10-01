039a99a0069421ade5b6f81405d4173a9184020a
Epafroditus George <cavidjaja@gmail.com>
2021-10-01 10:03:24 +0700

    up

---
+++ database/seeders/BankSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::unprepared("INSERT INTO banks (nama, code) VALUES ('KEB Hana','1000');");
+    }
+}
