3ef012d3a91acbd1d1876f65e3f7ee96985b2b30
George <71333847+georgeclm@users.noreply.github.com>
2021-12-04 21:51:29 +0700

    up

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -115,7 +115,6 @@ class Home extends Component
             }
         }
         // dd($incomeDiff);
-        $this->dispatchBrowserEvent('refresh-chart');
         $this->emit('thechart');
 
         return view('livewire.home', compact('income', 'spending', 'balance', 'prevIncome', 'prevSpending', 'incomeDiff', 'spendingDiff', 'spendingDiffPercent', 'incomeDiffPercent'));
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -301,230 +301,213 @@
     <script>
         window.addEventListener('refresh-chart', event => {
             thechart();
+            chart2();
         });
 
         @if (auth()->user()->all_transactions->count() != 0)
         function thechart(){
-
-        var x = window.matchMedia("(max-width: 700px)");
-        if (x.matches) {
-            var size = 10;
-        } else {
-            var size = 12;
-        }
-        // Set new default font family and font color to mimic Bootstrap's default styling
-        (Chart.defaults.global.defaultFontFamily = "Nunito"),
-        '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-        Chart.defaults.global.defaultFontColor = "#858796";
-
-        function number_format(number, decimals, dec_point, thousands_sep) {
-            // *     example: number_format(1234.56, 2, ',', ' ');
-            // *     return: '1 234,56'
-            number = (number + "").replace(",", "").replace(" ", "");
-            var n = !isFinite(+number) ? 0 : +number,
-                prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-                sep = typeof thousands_sep === "undefined" ? "." : thousands_sep,
-                dec = typeof dec_point === "undefined" ? "," : dec_point,
-                s = "",
-                toFixedFix = function(n, prec) {
-                    var k = Math.pow(10, prec);
-                    return "" + Math.round(n * k) / k;
-                };
-            // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-            s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
-            if (s[0].length > 3) {
-                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+            var x = window.matchMedia("(max-width: 700px)");
+            if (x.matches) {
+                var size = 10;
+            } else {
+                var size = 12;
             }
-            if ((s[1] || "").length < prec) {
-                s[1] = s[1] || "";
-                s[1] += new Array(prec - s[1].length + 1).join("0");
+            // Set new default font family and font color to mimic Bootstrap's default styling
+            (Chart.defaults.global.defaultFontFamily = "Nunito"),
+            '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+            Chart.defaults.global.defaultFontColor = "#858796";
+            function number_format(number, decimals, dec_point, thousands_sep) {
+                // *     example: number_format(1234.56, 2, ',', ' ');
+                // *     return: '1 234,56'
+                number = (number + "").replace(",", "").replace(" ", "");
+                var n = !isFinite(+number) ? 0 : +number,
+                    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+                    sep = typeof thousands_sep === "undefined" ? "." : thousands_sep,
+                    dec = typeof dec_point === "undefined" ? "," : dec_point,
+                    s = "",
+                    toFixedFix = function(n, prec) {
+                        var k = Math.pow(10, prec);
+                        return "" + Math.round(n * k) / k;
+                    };
+                // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+                s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
+                if (s[0].length > 3) {
+                    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+                }
+                if ((s[1] || "").length < prec) {
+                    s[1] = s[1] || "";
+                    s[1] += new Array(prec - s[1].length + 1).join("0");
+                }
+                return s.join(dec);
             }
-            return s.join(dec);
-        }
-
-        // Area Chart Example
-        var ctx = document.getElementById("myAreaChart");
-        var x = window.matchMedia("(max-width: 700px)");
-
-        var myLineChart = new Chart(ctx, {
-            type: "line",
-            data: {
-                labels: [
-                    "{{ substr(
-    now()->subMonth(4)->format('F'),
-    0,
-    3,
-) }}",
-                    "{{ substr(
-    now()->subMonth(3)->format('F'),
-    0,
-    3,
-) }}",
-                    "{{ substr(
-    now()->subMonth(2)->format('F'),
-    0,
-    3,
-) }}",
-                    "{{ substr(
-    now()->subMonth(1)->format('F'),
-    0,
-    3,
-) }}",
-                    "{{ substr(now()->format('F'), 0, 3) }}",
-                ],
-                datasets: [{
-                        label: "Spending",
-                        lineTension: 0.3,
-                        backgroundColor: "rgba(78, 115, 223, 0.05)",
-                        borderColor: "#e74a3b",
-                        pointRadius: 3,
-                        pointBackgroundColor: "#e74a3b",
-                        pointBorderColor: "#e74a3b",
-                        pointHoverRadius: 3,
-                        pointHoverBackgroundColor: "#e74a3b",
-                        pointHoverBorderColor: "#e74a3b",
-                        pointHitRadius: 10,
-                        pointBorderWidth: 2,
-                        data: [
-                            "{{ $prevSpending[4] }}",
-                            "{{ $prevSpending[3] }}",
-                            "{{ $prevSpending[2] }}",
-                            "{{ $prevSpending[1] }}",
-                            "{{ $spending }}",
-                        ],
+            // Area Chart Example
+            var ctx = document.getElementById("myAreaChart");
+            var x = window.matchMedia("(max-width: 700px)");
+            var myLineChart = new Chart(ctx, {
+                type: "line",
+                data: {
+                    labels: [
+                        "{{ substr(now()->subMonth(4)->format('F'),0,3) }}",
+                        "{{ substr(now()->subMonth(3)->format('F'),0,3) }}",
+                        "{{ substr(now()->subMonth(2)->format('F'),0,3) }}",
+                        "{{ substr(now()->subMonth(1)->format('F'),0,3) }}",
+                        "{{ substr(now()->format('F'), 0, 3) }}",
+                    ],
+                    datasets: [{
+                            label: "Spending",
+                            lineTension: 0.3,
+                            backgroundColor: "rgba(78, 115, 223, 0.05)",
+                            borderColor: "#e74a3b",
+                            pointRadius: 3,
+                            pointBackgroundColor: "#e74a3b",
+                            pointBorderColor: "#e74a3b",
+                            pointHoverRadius: 3,
+                            pointHoverBackgroundColor: "#e74a3b",
+                            pointHoverBorderColor: "#e74a3b",
+                            pointHitRadius: 10,
+                            pointBorderWidth: 2,
+                            data: [
+                                "{{ $prevSpending[4] }}",
+                                "{{ $prevSpending[3] }}",
+                                "{{ $prevSpending[2] }}",
+                                "{{ $prevSpending[1] }}",
+                                "{{ $spending }}",
+                            ],
+                        },
+                        {
+                            label: "Income",
+                            lineTension: 0.3,
+                            backgroundColor: "rgba(78, 115, 223, 0.05)",
+                            borderColor: "#1cc88a",
+                            pointRadius: 3,
+                            pointBackgroundColor: "#1cc88a",
+                            pointBorderColor: "#1cc88a",
+                            pointHoverRadius: 3,
+                            pointHoverBackgroundColor: "#1cc88a",
+                            pointHoverBorderColor: "#1cc88a",
+                            pointHitRadius: 10,
+                            pointBorderWidth: 2,
+                            data: [
+                                "{{ $prevIncome[4] }}",
+                                "{{ $prevIncome[3] }}",
+                                "{{ $prevIncome[2] }}",
+                                "{{ $prevIncome[1] }}",
+                                "{{ $income }}",
+                            ],
+                        }
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    scales: {
+                        xAxes: [{
+                            time: {
+                                unit: "date",
+                            },
+                            gridLines: {
+                                display: false,
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                maxTicksLimit: 7,
+                            },
+                        }, ],
+                        yAxes: [{
+                            ticks: {
+                                maxTicksLimit: 5,
+                                padding: 10,
+                                fontSize: size,
+                                // Include a dollar sign in the ticks
+                                callback: function(value, index, values) {
+                                    return "Rp." + number_format(value);
+                                },
+                            },
+                            gridLines: {
+                                color: "rgb(234, 236, 244)",
+                                zeroLineColor: "rgb(234, 236, 244)",
+                                drawBorder: false,
+                                borderDash: [2],
+                                zeroLineBorderDash: [2],
+                            },
+                        }, ],
                     },
-                    {
-                        label: "Income",
-                        lineTension: 0.3,
-                        backgroundColor: "rgba(78, 115, 223, 0.05)",
-                        borderColor: "#1cc88a",
-                        pointRadius: 3,
-                        pointBackgroundColor: "#1cc88a",
-                        pointBorderColor: "#1cc88a",
-                        pointHoverRadius: 3,
-                        pointHoverBackgroundColor: "#1cc88a",
-                        pointHoverBorderColor: "#1cc88a",
-                        pointHitRadius: 10,
-                        pointBorderWidth: 2,
-                        data: [
-                            "{{ $prevIncome[4] }}",
-                            "{{ $prevIncome[3] }}",
-                            "{{ $prevIncome[2] }}",
-                            "{{ $prevIncome[1] }}",
-                            "{{ $income }}",
-                        ],
-                    }
-                ],
-            },
-            options: {
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [{
-                        time: {
-                            unit: "date",
-                        },
-                        gridLines: {
-                            display: false,
-                            drawBorder: false,
-                        },
-                        ticks: {
-                            maxTicksLimit: 7,
-                        },
-                    }, ],
-                    yAxes: [{
-                        ticks: {
-                            maxTicksLimit: 5,
-                            padding: 10,
-                            fontSize: size,
-                            // Include a dollar sign in the ticks
-                            callback: function(value, index, values) {
-                                return "Rp." + number_format(value);
+                    legend: {
+                        display: false,
+                    },
+                    tooltips: {
+                        backgroundColor: "rgb(255,255,255)",
+                        bodyFontColor: "#858796",
+                        titleMarginBottom: 10,
+                        titleFontColor: "#6e707e",
+                        titleFontSize: 14,
+                        borderColor: "#dddfeb",
+                        borderWidth: 1,
+                        xPadding: 15,
+                        yPadding: 15,
+                        displayColors: false,
+                        intersect: false,
+                        mode: "index",
+                        caretPadding: 10,
+                        callbacks: {
+                            label: function(tooltipItem, chart) {
+                                var datasetLabel =
+                                    chart.datasets[tooltipItem.datasetIndex].label || "";
+                                return (
+                                    datasetLabel +
+                                    ": Rp." +
+                                    number_format(tooltipItem.yLabel)
+                                );
                             },
                         },
-                        gridLines: {
-                            color: "rgb(234, 236, 244)",
-                            zeroLineColor: "rgb(234, 236, 244)",
-                            drawBorder: false,
-                            borderDash: [2],
-                            zeroLineBorderDash: [2],
-                        },
-                    }, ],
-                },
-                legend: {
-                    display: false,
-                },
-                tooltips: {
-                    backgroundColor: "rgb(255,255,255)",
-                    bodyFontColor: "#858796",
-                    titleMarginBottom: 10,
-                    titleFontColor: "#6e707e",
-                    titleFontSize: 14,
-                    borderColor: "#dddfeb",
-                    borderWidth: 1,
-                    xPadding: 15,
-                    yPadding: 15,
-                    displayColors: false,
-                    intersect: false,
-                    mode: "index",
-                    caretPadding: 10,
-                    callbacks: {
-                        label: function(tooltipItem, chart) {
-                            var datasetLabel =
-                                chart.datasets[tooltipItem.datasetIndex].label || "";
-                            return (
-                                datasetLabel +
-                                ": Rp." +
-                                number_format(tooltipItem.yLabel)
-                            );
-                        },
                     },
                 },
-            },
-        });
+            });
+        }
+        thechart();
         @endif
 
         // Pie Chart Example
         @if (!auth()->user()->rekenings->isEmpty())
-
-            var ctx = document.getElementById("myPieChart");
-            var myPieChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-            labels: ["Deposito", "Mutual Funds (Reksadana)", "Stock", "P2P", 'Cash'],
-            datasets: [{
-            data: ["{{ auth()->user()->deposito_persen() }}",
-            "{{ auth()->user()->mutualfund_persen() }}",
-            "{{ auth()->user()->stock_persen() }}",
-            "{{ auth()->user()->p2p_persen() }}",
-            "{{ auth()->user()->saldo_persen() }}"
-            ],
-            backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#f6c23e', '#e74a3b'],
-            hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#dda20a', '#e02d1b'],
-            hoverBorderColor: "rgba(234, 236, 244, 1)",
-            borderWidth: [0, 0, 0, 0, 0]
-            }],
-            },
-            options: {
-            maintainAspectRatio: false,
-            tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            caretPadding: 10,
-            },
-            legend: {
-            display: true,
-            position: 'bottom',
-            },
-            cutoutPercentage: 80,
-            },
-            });
-        }
-        thechart();
+            function chart2(){
+                var ctx = document.getElementById("myPieChart");
+                var myPieChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                labels: ["Deposito", "Mutual Funds (Reksadana)", "Stock", "P2P", 'Cash'],
+                datasets: [{
+                data: ["{{ auth()->user()->deposito_persen() }}",
+                "{{ auth()->user()->mutualfund_persen() }}",
+                "{{ auth()->user()->stock_persen() }}",
+                "{{ auth()->user()->p2p_persen() }}",
+                "{{ auth()->user()->saldo_persen() }}"
+                ],
+                backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#f6c23e', '#e74a3b'],
+                hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#dda20a', '#e02d1b'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+                borderWidth: [0, 0, 0, 0, 0]
+                }],
+                },
+                options: {
+                maintainAspectRatio: false,
+                tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+                },
+                legend: {
+                display: true,
+                position: 'bottom',
+                },
+                cutoutPercentage: 80,
+                },
+                });
+            }
+        chart2();
         @endif
 
     </script>
