2d688114a946790d01537c4587b5e617371de09c
George <71333847+georgeclm@users.noreply.github.com>
2021-12-21 11:46:42 +0700

    up

--- resources/views/livewire/file-upload.blade.php
+++ resources/views/livewire/file-upload.blade.php
@@ -3,24 +3,32 @@
 <div class="mt-4 ml-5 mr-5">
 
     <div class="d-flex">
+        {{-- this mean if user is not logged in --}}
         @guest
             <a href="{{ route('login') }}" name="login" class="btn btn-primary">
                 Login
             </a>
         @endguest
+        {{-- if user is logged in --}}
         @auth
             <button wire:click="logout"" name=" logout" class="btn btn-danger mr-2">
                 Logout
             </button>
         @endauth
+        {{-- if the user is admin then act like superuser --}}
         @if (@auth()->user()->email == 'admin@example.com')
+            {{-- on the submit of the form wire submit prevent mean it will call the function uploadFile in FileUpload Class --}}
             <form wire:submit.prevent="uploadFile" enctype="multipart/form-data">
+                {{-- this wire model file will merge to the public variable $file in FileUpload class --}}
                 <input type="file" class="" wire:model.defer="file" required
                     id="upload{{ $fileIteration }}">
+                {{-- error message validation --}}
                 @error('file') <span class="error">{{ $message }}</span> @enderror
+                {{-- if image still upload on loading then this button will be remove --}}
                 <button wire:loading.remove type="submit" class="btn btn-primary">
                     Add
                 </button>
+                {{-- if image still upload on loading then this button will show --}}
                 <button wire:loading wire:target="file" class="btn btn-primary" type="button" disabled>
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                     Loading...
@@ -41,6 +49,7 @@
             </tr>
         </thead>
         <tbody>
+            {{-- this is to loop every fileupload --}}
             @foreach ($fileuploads as $key => $fileupload)
                 <tr>
                     <th scope="row">{{ $key + 1 }}</th>
@@ -49,12 +58,14 @@
                     <td>{{ $fileupload->size }}
                     </td>
                     <td>
+                        {{-- on button click this will call the function downloadImage in FileUpload class with the id as param --}}
                         <button class="btn btn-primary" wire:click="downloadImage({{ $fileupload->id }})">
                             Download
                         </button>
                     </td>
                     <td>
                         <div class="d-flex">
+                            {{-- super user here add '@' before variable so if the var doesnt exist it will return false --}}
                             @if (@auth()->user()->email == 'admin@example.com')
                                 <form class="" wire:submit.prevent="updateFile({{ $fileupload->id }})">
                                     <input type="file" class="" wire:model.defer="fileUpdate" required>
